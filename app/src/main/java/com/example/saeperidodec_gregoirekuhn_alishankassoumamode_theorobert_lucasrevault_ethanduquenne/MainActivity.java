4fed934e56ca785f9092a0bb288c90828b01cfe9
Gregoire <gregoire.kuhn@outlook.fr>
2025-02-14 11:09:15 +0100

    Ajout du bon logo dans tout les formats grace a icon.kitchen

--- app/src/main/java/com/example/saeperidodec_gregoirekuhn_alishankassoumamode_theorobert_lucasrevault_ethanduquenne/MainActivity.java
+++ app/src/main/java/com/example/saeperidodec_gregoirekuhn_alishankassoumamode_theorobert_lucasrevault_ethanduquenne/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.sae.R;
+
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
