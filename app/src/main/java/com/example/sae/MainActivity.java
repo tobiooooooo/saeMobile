789e1557a5bcecdd43e95d23511c3c9b85a2e157
Gregoire <gregoire.kuhn@outlook.fr>
2025-02-15 12:37:11 +0100

    ajout hint pout les edit text ajout boutton se connecter

--- app/src/main/java/com/example/sae/MainActivity.java
+++ app/src/main/java/com/example/sae/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.sae;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.sae.Login;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button log_in_btn = (Button) findViewById(R.id.log_in_btn);
+
+        log_in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
         });
     }
 }
