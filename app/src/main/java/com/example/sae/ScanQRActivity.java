1ec0b9506fb6dcd056589d59deeaf225c625182a
lucas revault <123903340+tobiooooooo@users.noreply.github.com>
2025-02-18 19:21:47 +0100

    Update ScanQRActivity.java
    qr

--- app/src/main/java/com/example/sae/ScanQRActivity.java
+++ app/src/main/java/com/example/sae/ScanQRActivity.java
@@ -1,15 +1,24 @@
 package com.example.sae;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class ScanQRActivity extends AppCompatActivity {
 
@@ -28,7 +37,6 @@ public class ScanQRActivity extends AppCompatActivity {
         Button logInBtn = (Button) findViewById(R.id.log_in_btnn);
 
 
-
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,5 +44,37 @@ public class ScanQRActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        Button scanQRButton;
+        scanQRButton = findViewById(R.id.scan_qr_button);
+        scanQRButton.setOnClickListener(v -> startQRScanner());
+
     }
+
+
+    private void startQRScanner() {
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setPrompt("Scannez un QR Code");
+        integrator.setBeepEnabled(true);
+        integrator.setOrientationLocked(false);
+        integrator.setCaptureActivity(CaptureActivity.class);
+        integrator.initiateScan();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null && result.getContents() != null) {
+            // Redirection après scan réussi
+            Intent intent = new Intent(this, DonationActivity.class);
+            intent.putExtra("qrData", result.getContents());
+            startActivity(intent);
+        }
+    }
+
+
 }
