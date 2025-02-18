9d18a0bf057c39c8e118d4e89d875e801a650fd4
tobio <lucasrevault32@gmail.com>
2025-02-18 22:31:12 +0100

    menu burger

--- app/src/main/java/com/example/sae/MainActivity.java
+++ app/src/main/java/com/example/sae/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.sae;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -28,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         Button discoverBtn = (Button) findViewById(R.id.discover_btn);
         Button logInBtn = (Button) findViewById(R.id.log_in_btn);
         Button scanQRButton = findViewById(R.id.btn_qr_code);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        ImageButton menuButton = findViewById(R.id.menu_button);
 
         makeDonationBtn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -42,6 +51,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DiscoverActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
@@ -61,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
 
 
     }
