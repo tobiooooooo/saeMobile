5d7c399fabb7892f5990140d57af4727ba0e40ea
Gregoire <gregoire.kuhn@outlook.fr>
2025-02-20 18:23:59 +0100

    login admin faut changer string apres

--- app/src/main/java/com/example/sae/Login.java
+++ app/src/main/java/com/example/sae/Login.java
@@ -1,6 +1,12 @@
 package com.example.sae;
 
+import static com.example.sae.R.id.button_connection;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Login extends AppCompatActivity {
+
+    List<String> admin = List.of("greg","theo","lucas","ethan","ali-shan");
+    List<String> adminpsw = List.of("gregmdp","theomdp","lucasmdp","ethanmdp","ali-shanmdp");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,29 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button connectButton = findViewById(R.id.button_connection);
+        EditText emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        EditText pswEditText = findViewById(R.id.editTextTextPassword);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String psw = pswEditText.getText().toString();
+                Integer connection = 0;
+                for(int i=0;i<admin.size();i++){
+                    if(email.equals(admin.get(i))&&(psw.equals(adminpsw.get(i)))){
+                        Toast.makeText(getApplicationContext(),"connection réussi", Toast.LENGTH_LONG).show();
+                        connection=1;
+                    }
+                }
+                if (connection==0){
+                    Toast.makeText(getApplicationContext(),"connection failed",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
