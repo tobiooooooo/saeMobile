51d91893811e8849d495a6d9337bd173877298cd
Théo <theo.t.robert@gmail.com>
2025-02-26 14:35:51 +0100

    suppression de DiscoverActivity.java, remplacé par activity_selection_assos.xml qui gèrera la liste des assos

--- app/src/main/java/com/example/sae/MainActivity.java
+++ app/src/main/java/com/example/sae/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         discoverBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DiscoverActivity.class);
+                Intent intent = new Intent(MainActivity.this, selection_assos_activity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/sae/selection_assos_activity.java
+++ app/src/main/java/com/example/sae/selection_assos_activity.java
@@ -7,18 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class DiscoverActivity extends AppCompatActivity {
+public class selection_assos_activity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_discover);
+        setContentView(R.layout.activity_selection_assos);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
